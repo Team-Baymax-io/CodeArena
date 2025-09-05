b5dec93d722ebb05645c6a706b26a510504821ea
binay <binay885b@gmail.com>
2025-09-05 23:04:30 +0530

    dev commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Home from "./components/Home";
 
-function App() {}
+function App() {
+  return (
+    <div className="flex h-screen w-screen">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
 
 export default App;
