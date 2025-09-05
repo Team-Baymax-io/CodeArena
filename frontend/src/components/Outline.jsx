df45cfdf43bba2a7e9749af4de6b54115b2597f8
cyasha05 <cyasha05@gmail.com>
2025-09-06 00:05:08 +0530

    added index.js for components

---
+++ frontend/src/components/Outline.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Outline = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default Outline;
---
+++ frontend/src/components/index.js
@@ -0,0 +1,19 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Feature from "./Feature";
+import Hero from "./Hero";
+import Product from "./Product";
+import ProductItem from "./ProductItem";
+import FeatureCard from "./FeatureCard";
+import Error from "./404Error";
+
+export {
+  Header,
+  Footer,
+  Feature,
+  FeatureCard,
+  Hero,
+  Product,
+  ProductItem,
+  Error,
+};
